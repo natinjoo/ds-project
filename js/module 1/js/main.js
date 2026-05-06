c14c95f7f073b3b82b66e1049636b9c8388306e2
natinjoo <ronat527@gmail.com>
2026-05-06 19:19:36 +0200

    commit

---
+++ js/module 1/js/main.js
@@ -0,0 +1,39 @@
+console.log("goodbye")
+
+var x="1"
+
+var b=20+10
+
+console.log(b)
+document.write(b)
+
+var a=99
+
+a++;
+console.log(a)
+
+var y=20
+var q=y**2
+
+console.log(q)
+
+var z=12
+z+=3    
+console.log("z")
+
+var r=5
+var t=5
+console.log(r==t)
+
+var u=5
+var i=5
+console.log(u!=i)
+
+var emri="digital"
+var mbiemri="school"
+var fullname=emri+" "+mbiemri
+
+console.log(fullname)
+
+var school="digital"
+console.log("hello from ${school}")
